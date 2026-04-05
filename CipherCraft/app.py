cabbb39662faaf4c7a816a77688e4d3d423c10e6
Prathamesh Penshanwar <128643250+PRATHAM777P@users.noreply.github.com>
2026-04-05 18:57:08 +0530

    Update app.py

--- CipherCraft/app.py
+++ CipherCraft/app.py
@@ -1,1 +1,7 @@
-
+def get_history(user):
+    conn = sqlite3.connect("database.db")
+    c = conn.cursor()
+    c.execute("SELECT action, result FROM history WHERE user=?", (user,))
+    data = c.fetchall()
+    conn.close()
+    return data
